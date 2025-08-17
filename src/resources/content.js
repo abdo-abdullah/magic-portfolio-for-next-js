cce8b93c2d0e77836096971e19aa1b1c2ad5eee0
Abdo_Abdullah <114471215+abdo-abdullah@users.noreply.github.com>
2025-08-17 13:52:54 +0300

    Update content.js

--- src/resources/content.js
+++ src/resources/content.js
@@ -1,8 +1,8 @@
 import { Logo } from "@once-ui-system/core";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "Abdo",
+  lastName: "Abdullah",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
